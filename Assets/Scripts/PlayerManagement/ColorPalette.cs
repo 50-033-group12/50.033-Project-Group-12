3e97e649153d881b5d1f4814f4f45da6f6c99c29
Chester Koh <chester8991@gmail.com>
2021-08-17 16:00:06 +0800

    add color picking in selection menu, allow up/down movement

---
+++ Assets/Scripts/PlayerManagement/ColorPalette.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Team Color Palette")]
+public class ColorPalette : ScriptableObject
+{
+    public Color[] colors;
+}
